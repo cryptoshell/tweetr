296c736260973ece92de2e68a504c593b21d346d
Yelim Oh <yelim.oh1@gmail.com>
2017-07-13 23:15:49 +0000

    Changed in-memory database to MongoDB

---
+++ mongo-example.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const {MongoClient} = require("mongodb");
+const MONGODB_URI = 'mongodb://localhost:27017/tweeter';
+
+MongoClient.connect(MONGODB_URI, (err, db) => {
+  if (err) {
+    console.log(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+  }
+
+  // We have a connection to the "tweeter" db
+  console.log(`Connected to mongodb: ${MONGODB_URI}`);
+
+  function getTweets(callback) {
+    db.collection("tweets").find().toArray(callback);
+  }
+
+  getTweets((err, tweets) => {
+    if (err) throw err;
+
+    console.log("Logging each tweet:");
+    for (let tweet of tweets) {
+      console.log(tweet);
+    }
+
+    db.close();
+  });
+
+});
